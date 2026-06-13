9bc6c9af5ab6797619ed1dc4a395b3fbb13f12ea
cesarleyto <cezsar@gmail.com>
2026-06-13 12:43:32 -0600

    Actualización de estilos visuales

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()], // <--- ¡AQUÍ ESTÁ EL CAMBIO! Faltaba esta coma
+  integrations: [icon(), sitemap()], // <--- ¡AQUÍ ESTÁ EL CAMBIO! Faltaba esta coma
   site: 'https://yaga.haus',
 });
